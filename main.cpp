d642a91c008c545e32118a8bd883e8891c1d013e
kennedyengineering <bdogkennedy@hotmail.com>
2019-05-16 12:41:18 -0700

    basic collision system

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <array>
 #include <iostream>
+#include <cmath>
 
 class Object : public sf::CircleShape {
 private:
@@ -15,7 +16,57 @@ public:
 		sf::CircleShape::setFillColor(sf::Color(200,20,20));
 	}
 
-	void updatePosition() { 
+	void detectCollision(sf::RenderWindow &window, std::vector<Object> &objectVector) {
+		//detect collision
+		//change velocity
+		
+		//detect walls
+		const double winSizeX = window.getSize().x;
+		const double winSizeY = window.getSize().y;
+		const double objPosX = sf::CircleShape::getPosition().x;
+		const double objPosY = sf::CircleShape::getPosition().y;
+		const double radius = sf::CircleShape::getRadius();
+		const double centerX = objPosX + radius;
+		const double centerY = objPosY + radius;
+
+		if (winSizeX <= (objPosX+(2*radius))) 
+			_velocity[0] *= -1; //perfectly elastic collision
+		if (objPosX <= 0)
+			_velocity[0] *= -1;
+
+		if (winSizeY <= (objPosY+(2*radius)))
+			_velocity[1] *= -1;
+		if (objPosY <= 0)
+			_velocity[1] *= -1;
+
+		//detect other Objects
+		for (int i = 0; i < objectVector.size(); i++) {
+			if (&objectVector[i] == this) //check for self
+				continue;
+			else {
+				//detect collision
+				double otherPosX = objectVector[i].getPosition().x;
+				double otherPosY = objectVector[i].getPosition().y;
+				double otherRadius = objectVector[i].getRadius();
+				double otherCenterX = otherPosX + otherRadius;
+				double otherCenterY = otherPosY + otherRadius;
+
+				double distanceX = std::abs(otherCenterX - centerX);
+				double distanceY = std::abs(otherCenterY - centerY);
+				double distance = std::sqrt((distanceX*distanceX) + (distanceY*distanceY));
+
+				if (distance <= (radius+otherRadius)) {
+					//collision detected
+					//work out new velocity
+					_velocity[0] *= -1;
+					_velocity[1] *= -1;
+
+				}		
+			}
+		}		
+	}
+
+	void updatePosition() {
 		sf::CircleShape::move(_velocity[0], _velocity[1]);	
 	}
 };
@@ -38,6 +89,7 @@ public:
 		windowReference->clear(sf::Color::Black);
 		
 		for (int i = 0; i < objectVector.size(); i++) {
+			objectVector[i].detectCollision(*windowReference, objectVector);
 			objectVector[i].updatePosition();
 			windowReference->draw(objectVector[i]);
 		}
@@ -65,7 +117,8 @@ int main() {
 	*/
 
 	//radius, mass, position, velocity
-	engine.createObject(10.0, 10.0, std::array<double, 2> {20,20}, std::array<double, 2> {0.5,0});
+	engine.createObject(10.0, 10.0, std::array<double, 2> {20,20}, std::array<double, 2> {0.5,0.0});
+	engine.createObject(20.0, 5.0, std::array<double, 2> {20, 80}, std::array<double, 2> {0.5,1.0});
 
 	while (window.isOpen()) {
 		sf::Event event;
